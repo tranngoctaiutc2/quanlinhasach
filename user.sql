f82db4280db03c6cb7d5de10a27296c7bf1daf18
tranngoctaiutc2 <102678658+tranngoctaiutc2@users.noreply.github.com>
2023-06-01 17:23:49 +0700

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,2 @@
+select * from book.user;
+insert into user(iduser,name,sex,email,password,birth) values ('adsf','hao',1,'nhathao@gmail.com','hao','2003-11-11');
---
+++ userview.sql
@@ -0,0 +1,2 @@
+CREATE VIEW `view1` AS
+select * from user;
